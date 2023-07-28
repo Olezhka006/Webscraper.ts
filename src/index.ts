36bfec8ad79f4fd140e8dfd7101e4a815af14307
Roman <140193303+Belxz777@users.noreply.github.com>
2023-07-28 21:19:37 +0400

    last

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import cheerio from 'cheerio';
-
+interface city  {
+    name:string,
+    created:string,
+    humans:string
+    }
 export class CapitalCityScraper {
     async scrapeCity(url: string) {
         const response = await axios.get(url);
@@ -9,13 +13,27 @@ export class CapitalCityScraper {
         const $ = cheerio.load(html);
 
         const cityName = $('.mergedtoprow th:contains(Country) + td').text().trim();
-        console.log(cityName);
+        const createdAt = $('.mergedtoprow th:contains(Founded) + td').text().trim()
+        const humansamout = $('.mergedrow th:contains(Total) + td').text().trim()
+        const humanAmount = Number(humansamout)
+        const flagPageLink = $('.mergedtoprow a.image + div:contains(Flag)').prev().attr('href')!;
+        const flagPageUrl = new URL(flagPageLink, url).toString();
+        console.log(flagPageUrl);
+        console.log(cityName)
+        const City :city= {
+            name:cityName,
+            created:createdAt,
+            humans:humansamout
+               }
+               return City;
+               
     }
 }
-
 async function main() {
     const scraper = new CapitalCityScraper();
-    await scraper.scrapeCity("https://en.wikipedia.org/wiki/Izhevsk");
+    const City = await scraper.scrapeCity("https://en.wikipedia.org/wiki/Izhevsk");
+    console.log(City);
 }
+
 
 main();
